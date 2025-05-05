e2002086e345e6d9c861f5b1f509fd00a24e4519
LopDaBop <pteyra@gmail.com>
2025-05-05 09:44:14 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,16 +1,15 @@
-// auth.js
-(function enforceLogin() {
-    const currentPage = window.location.pathname;
-
-    // Pages that shouldn't require login
-    const publicPages = ['login.html', 'register.html'];
-
-    // If the current page is public, skip the check
-    if (publicPages.some(p => currentPage.endsWith(p))) return;
-
-    // Otherwise, check for a logged in user
+function checkAuthentication() {
     const loggedInUser = localStorage.getItem("loggedInUser");
     if (!loggedInUser) {
-        window.location.replace("login.html");
+        window.location.href = "login.html";
     }
-})();
+}
+
+function logoutUser() {
+    localStorage.removeItem("loggedInUser");
+    window.location.href = "login.html";
+}
+
+function getCurrentUser() {
+    return JSON.parse(localStorage.getItem("loggedInUser"));
+}
